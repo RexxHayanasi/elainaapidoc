097863d6a65cf293610a06f390b07be6a003a4dd
RexxzynXD <150516773+RexxHayanasi@users.noreply.github.com>
2025-07-04 00:15:15 +0700

    Update allDownloader.js

--- routes/allDownloader.js
+++ routes/allDownloader.js
@@ -7,6 +7,28 @@ const qs = require('qs')
 const yts = require('yt-search');
 const { createDecipheriv } = require('crypto')
 
+const audio = [92, 128, 256, 320];
+const video = [144, 360, 480, 720, 1080];
+
+const hexcode = (hex) => Buffer.from(hex, 'hex');
+const decode = (enc) => {
+    try {
+        const secret_key = 'C5D58EF67A7584E4A29F6C35BBC4EB12';
+        const data = Buffer.from(enc, 'base64');
+        const iv = data.slice(0, 16);
+        const content = data.slice(16);
+        const key = hexcode(secret_key);
+
+        const decipher = createDecipheriv('aes-128-cbc', key, iv);
+        let decrypted = Buffer.concat([decipher.update(content), decipher.final()]);
+
+        return JSON.parse(decrypted.toString());
+    } catch (error) {
+      throw new Error(error.message);
+    }
+};
+
+
 async function mediafire(url) {
 	let res = await axios.get(url)
 	let get = cheerio.load(res.data)
@@ -173,42 +195,49 @@ function sfilemobi(url) {
 	})
 }
 
-async function ytmp3(url) {
-  return new Promise((resolve, reject) => {
+async function ytmp3(link, quality, value) {
     try {
-      const id = yt.getVideoID(url)
-      const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
-      .then((data) => {
-        let pormat = data.formats
-        let audio = []
-        for (let i = 0; i < pormat.length; i++) {
-          if (pormat[i].mimeType == 'audio/webm; codecs=\"opus\"') {
-            let aud = pormat[i]
-            audio.push(aud.url)
+        const cdn = (await axios.get("https://media.savetube.me/api/random-cdn")).data.cdn;
+        const infoget = (await axios.post(
+          'https://' + cdn + '/v2/info',
+          { url: link },
+          {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36',
+                'Referer': 'https://yt.savetube.me/1kejjj1?id=362796039'
+            }
           }
-        }
-        const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
-        const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
-        const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
-        const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
-        const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
-        
-        const result = {
-          title: title,
-          thumb: thumb,
-          channel: channel,
-          published: published,
-          views: views,
-          url: audio[0]
-        }
-        return(result)
-      })
-      resolve(yutub)
+        )).data;
+        const info = decode(infoget.data);
+        const response = (await axios.post(
+          'https://' + cdn + '/download',
+          {
+            'downloadType': value,
+            'quality': `${quality}`,
+            'key': info.key
+          },
+          {
+            headers: {
+                'Content-Type': 'application/json',
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36',
+                'Referer': 'https://yt.savetube.me/start-download?from=1kejjj1%3Fid%3D362796039'
+            }
+          }
+        )).data;
+        return {
+            status: true,
+            quality: `${quality}${value === "audio" ? "kbps" : "p"}`,
+            availableQuality: value === "audio" ? audio : video,
+            url: response.data.downloadUrl,
+            filename: `${info.title} (${quality}${value === "audio" ? "kbps).mp3" : "p).mp4"}`
+        };
     } catch (error) {
-        reject(error);
-      }
-      console.log(error)
-  })
+        console.error("Converting error:", error);
+        return {
+            status: false,
+            message: "Converting error"
+        };
+    }
 }
 
 async function ytmp4(url) {
